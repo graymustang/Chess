783e3c39cd9d092d5a1ca68b75ce9f26bab85c5e
Bjorn <graybjo0@byu.edu>
2026-01-19 21:54:03 -0700

    Added moves for pawn

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -82,6 +82,7 @@ public class ChessPiece {
 
             case KNIGHT -> addKnightMoves(board, myPosition, moves);
             case KING -> addKingMoves(board, myPosition, moves);
+            case PAWN -> addPawnMoves(board, myPosition, moves);
 
 
 
@@ -91,6 +92,63 @@ public class ChessPiece {
         }
 
         return moves;
+    }
+
+    private void addPawnMoves(ChessBoard board, ChessPosition start, Collection<ChessMove> moves){
+        int direction;
+        int startRow;
+
+        if (this.getTeamColor() == ChessGame.TeamColor.WHITE){
+            direction =1;
+            startRow= 2;
+        } else {
+            direction = -1;
+            startRow = 7;
+        }
+        int r = start.getRow();
+        int c = start.getColumn();
+
+        ChessPosition oneForward = new ChessPosition(r+ direction, c);
+        if (inBounds(oneForward) && board.getPiece(oneForward) == null){
+           addPawnMoveOrPromotions(start, oneForward, moves);
+
+            ChessPosition twoForward = new ChessPosition(r+2 * direction, c);
+            if(r == startRow && inBounds(twoForward) && board.getPiece(twoForward) == null){
+                addPawnMoveOrPromotions(start, twoForward, moves);
+            }
+        }
+
+        ChessPosition diagLeft= new ChessPosition(r +direction, c-1);
+        if (inBounds(diagLeft)){
+            ChessPiece target = board.getPiece(diagLeft);
+            if (target != null && target.getTeamColor() !=this.getTeamColor()){
+                addPawnMoveOrPromotions(start, diagLeft, moves);
+            }
+        }
+
+        ChessPosition diagRight = new ChessPosition(r +direction, c+1);
+        if (inBounds(diagRight)){
+            ChessPiece target = board.getPiece(diagRight);
+            if(target != null && target.getTeamColor()!= this.getTeamColor()){
+                addPawnMoveOrPromotions(start, diagRight, moves);
+            }
+        }
+
+
+    }
+    private void addPawnMoveOrPromotions(ChessPosition start, ChessPosition end, Collection<ChessMove> moves){
+        int endRow = end.getRow();
+        if( endRow == 8 || endRow == 1){
+            moves.add(new ChessMove(start, end, PieceType.QUEEN));
+            moves.add(new ChessMove(start, end, PieceType.ROOK));
+            moves.add(new ChessMove(start, end, PieceType.BISHOP));
+            moves.add(new ChessMove(start, end, PieceType.KNIGHT));
+        } else {
+            moves.add(new ChessMove(start, end, null));
+        }
+    }
+    private boolean inBounds(ChessPosition pos){
+        return pos.getRow() >= 1 && pos.getRow() <= 8 && pos.getColumn() >= 1 &&pos.getColumn()<= 8;
     }
 
     private void addKnightMoves(ChessBoard board, ChessPosition start, Collection<ChessMove> moves){
@@ -116,8 +174,8 @@ public class ChessPiece {
         }
 
     }
-    private void addKingMoves(ChessBoard board, ChessPosition start, Collection<ChessMove> moves){
-        int [][] offsets = {
+    private void addKingMoves(ChessBoard board, ChessPosition start, Collection<ChessMove> moves) {
+        int[][] offsets = {
                 {1, 0}, {-1, 0}, {0, 1}, {0, -1},
                 {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
         };
@@ -125,14 +183,14 @@ public class ChessPiece {
         for (int[] off : offsets) {
             int r = start.getRow() + off[0];
             int c = start.getColumn() + off[1];
-            if ( r< 1 || r > 8 || c<1 || c > 8){
+            if (r < 1 || r > 8 || c < 1 || c > 8) {
                 continue;
             }
 
             ChessPosition next = new ChessPosition(r, c);
             ChessPiece onSquare = board.getPiece(next);
 
-            if (onSquare == null || onSquare.getTeamColor()!= this.getTeamColor()){
+            if (onSquare == null || onSquare.getTeamColor() != this.getTeamColor()) {
                 moves.add(new ChessMove(start, next, null));
             }
         }
