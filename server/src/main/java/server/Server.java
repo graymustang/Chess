5e2de545ede498d93c606845482b8dea065dda70
Bjorn <graybjo0@byu.edu>
2026-02-20 16:52:05 -0700

    Add more code to the Server.java file

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,11 @@
 package server;
 
-import io.javalin.*;
+import io.javalin.Javalin;
+import dataaccess.*;
+import org.eclipse.jetty.server.Authentication;
+import service.*;
+
+import java.security.Provider;
 
 public class Server {
 
@@ -10,6 +15,46 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        //in memory db and DAOs
+        MemoryDatabase mem = new MemoryDatabase();
+        UserDAO userDAO = new MemoryUserDAO(mem);
+        AuthDAO authDAO = new MemoryAuthDAO(mem);
+        GameDAO gameDAO = new MemoryGameDAO(mem);
+
+        //services
+        UserService userService = new UserService(userDAO, authDAO);
+        GameService gameService = new GameService(gameDAO, authDAO);
+        ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
+
+        //handlers
+        UserHandler userHandler = new UserHandler(userService);
+        GameHandler gameHandler = new GameHandler(gameService);
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
+        //endpoints
+        javalin.delete("/db", clearHandler::clear);
+
+        javalin.post("/user", userHandler::register);
+
+        javalin.post("/session", userHandler::login);
+        javalin.delete("/session", userHandler::logout);
+
+        javalin.get("/game", gameHandler::list);
+        javalin.post("/game", gameHandler::create);
+        javalin.put("/game", gameHandler::join);
+
+        //exception handling
+        javalin.exception(ServiceException.class, (e, ctx) -> {
+            ctx.status(e.status()).result(JsonUtil.GSON.toJson(new ErrorResult(e.getMessage())));
+        });
+
+        javalin.exception(Exception.class, (e, ctx) -> {
+            ctx.status(500).result(JsonUtil.GSON.toJson(new ErrorResult("Error: " + e.getMessage())));
+        });
+
+
+
+
 
     }
 
@@ -19,6 +64,7 @@ public class Server {
     }
 
     public void stop() {
+
         javalin.stop();
     }
 }
