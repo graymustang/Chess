5a8e4d26f95d71f4424bd9dbb16cdf6cddc67098
Bjorn <graybjo0@byu.edu>
2026-03-02 16:55:42 -0700

    Create MySqlAuthDAO.java and add code

---
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -0,0 +1,53 @@
+package dataaccess;
+
+import model.UserData;
+import java.sql.SQLException;
+
+public class MySqlUserDAO implements UserDAO {
+    @Override
+    public UserData getUser(String username) throws DataAccessException{
+        if (username == null){
+            return null;
+        }
+
+        try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement("SELECT username, password, email FROM user WHERE username=?")) {
+
+            ps.setString(1, username);
+            try (var rs = ps.executeQuery()){
+                if (!rs.next()) {
+                    return null;
+                }
+                return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+            }
+        } catch (SQLException e){
+            throw new DataAccessException("failed to get user", e);
+        }
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException{
+        if (user == null){
+            throw new DataAccessException("user is null");
+        }
+
+        try (var conn = DatabaseManager.getConnection();
+        var ps = conn.prepareStatement("INSERT into user (username, password, email) values(?, ?, ?)")){
+            ps.setString(1, user.username());
+            ps.setString(2, user.password());
+            ps.setString(3, user.email());
+        } catch (SQLException e){
+            throw new DataAccessException("failed to create user", e);
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement("TRUNCATE TABLE user")) {
+            ps.executeUpdate();
+        } catch (SQLException e){
+            throw new DataAccessException("failed to clear users", e);
+        }
+    }
+}
