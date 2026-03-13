198b8bdbd61dbaa41d4b8a2c3b7cc5edb4d23b27
Bjorn <graybjo0@byu.edu>
2026-03-12 21:47:05 -0600

    Moved three files into shared, and added code into ClientMain.java

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -3,8 +3,15 @@ package client;
 import chess.*;
 
 public class ClientMain {
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        var port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        var serverFacade = new ServerFacade(port);
+        var repl = new Repl(serverFacade);
+        repl.run();
     }
 }
